2a9fd7c8aec3973afd4934373f6049ed1005c85d
Jean GC <jeanpoolgarcia_97@hotmail.com>
2026-03-16 13:02:01 -0600

    build: create table to save posts

---
+++ cicd/infra/dynamodb/build.sh
@@ -0,0 +1,8 @@
+awslocal dynamodb create-table \
+    --table-name post \
+    --attribute-definitions AttributeName=id,AttributeType=S \
+    --key-schema AttributeName=id,KeyType=HASH \
+    --billing-mode PAY_PER_REQUEST
+
+# Verificamos que la tabla se ha creado correctamente
+awslocal dynamodb list-tables
