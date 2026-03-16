a06a091ff6a7633465cdfdf2e9985c86b082da2c
Jean GC <jeanpoolgarcia_97@hotmail.com>
2026-03-16 13:03:39 -0600

    build: create execution role with a policy, create lambda function and atach role

---
+++ cicd/infra/lambda/create_exec_role.sh
@@ -0,0 +1,19 @@
+
+SCRIPT_DIR=$(dirname "$0")
+
+# Crea la política de IAM con los permisos necesarios para que la función Lambda 
+# pueda acceder al servicio DynamoDB y escribir en CloudWatch Logs
+awslocal iam create-policy \
+    --policy-name thumbnail-lambda-policy \
+    --policy-document file://$SCRIPT_DIR/lambda-policy.json
+
+# Crea el rol de ejecución para la función Lambda y adjunta una trust policy
+# que permita a  Lambda asumir el rol
+awslocal iam create-role \
+    --role-name thumbnail-lambda-execution-role \
+    --assume-role-policy-document file://$SCRIPT_DIR/trust-policy.json
+
+# Adjunta la política de permisos al rol de ejecución
+awslocal iam attach-role-policy \
+    --role-name thumbnail-lambda-execution-role \
+    --policy-arn arn:aws:iam::000000000000:policy/thumbnail-lambda-policy
